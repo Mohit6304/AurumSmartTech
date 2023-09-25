b1bc439aaf78b2cb543a437cce23a7b0467672d5
bvsaimanvith <119737652+bvsaimanvith@users.noreply.github.com>
2023-09-25 20:39:08 +0530

    Update inherit.js

--- inherit.js
+++ inherit.js
@@ -3,16 +3,12 @@ class MyHeader extends HTMLElement
 {
     connectedCallback() 
     {
-      this.innerHTML = ` <!-- Announcement Bar -->
-      <div class="announcement-bar">
-          <p>"Empowering Industries Through Innovative Electronics Solutions - Discover Excellence Today!"</p>
-      </div>
-  
+      this.innerHTML = ` 
       <!-- Header Section -->
       <header>
           <div class="header-content">
               <div class="logo" >
-              <a href="/"><img src="/images/logo-removebg-preview.png" alt="Company Logo"></a>
+              <a href="/"><img src="/images/lo.png" alt="Company Logo"></a>
               </div>
               <nav>
                   <ul>
@@ -40,29 +36,29 @@ class MyFooter extends HTMLElement {
         <div class="row">
           <div class="col-sm-12 col-md-6">
             <h6>About</h6>
-            <p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
+            <p class="text-justify">Aurum Smart Tech specializes in industrial automation and cutting-edge technology solutions, from UHF-RFID to embedded firmware development, ensuring top-notch product engineering and testing for a defect-free final product.We are driven by a passion for innovation and a commitment to delivering excellence in every project we undertake.</p>
           </div>
 
           <div class="col-xs-6 col-md-3">
-            <h6>Categories</h6>
+            <h6>Projects</h6>
             <ul class="footer-links">
-              <li><a href="http://scanfcode.com/category/c-language/">C</a></li>
-              <li><a href="http://scanfcode.com/category/front-end-development/">UI Design</a></li>
-              <li><a href="http://scanfcode.com/category/back-end-development/">PHP</a></li>
-              <li><a href="http://scanfcode.com/category/java-programming-language/">Java</a></li>
-              <li><a href="http://scanfcode.com/category/android/">Android</a></li>
-              <li><a href="http://scanfcode.com/category/templates/">Templates</a></li>
+              <li><a href="http://scanfcode.com/category/c-language/">Project-1</a></li>
+              <li><a href="http://scanfcode.com/category/front-end-development/">Project-2</a></li>
+              <li><a href="http://scanfcode.com/category/back-end-development/">Project-3</a></li>
+              <li><a href="http://scanfcode.com/category/java-programming-language/">Project-4</a></li>
+              <li><a href="http://scanfcode.com/category/android/">Project-5</a></li>
+              <li><a href="http://scanfcode.com/category/templates/">Project-6</a></li>
             </ul>
           </div>
 
           <div class="col-xs-6 col-md-3">
             <h6>Quick Links</h6>
             <ul class="footer-links">
-              <li><a href="http://scanfcode.com/about/">About Us</a></li>
-              <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
-              <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a></li>
-              <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
-              <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
+              <li><a href="/pages/about-us.html">About Us</a></li>
+              <li><a href="/pages/contact.html">Contact Us</a></li>
+              <li><a href="/pages/services.html">Services</a></li>
+              <li><a href="/pages/products.html">Products</a></li>
+              <li><a href="/pages/team.html">Team</a></li>
             </ul>
           </div>
         </div>
@@ -71,8 +67,7 @@ class MyFooter extends HTMLElement {
       <div class="container">
         <div class="row">
           <div class="col-md-8 col-sm-6 col-xs-12">
-            <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by 
-         <a href="#">Scanfcode</a>.
+            <p class="copyright-text">Copyright © 2023 <a href="#">Aurum Smart Tech</a>. All Rights Reserved.
             </p>
           </div>
 
